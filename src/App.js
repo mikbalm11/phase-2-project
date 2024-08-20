327b47ef9893db5d1795217c1415fb5090a75c22
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-08-20 00:25:32 -0400

    basic fetch request!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, {useState, useEffect} from'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [movie, setMovies] = useState();
+
+  useEffect(() => {
+    fetch('https://www.omdbapi.com/?t=batman_begins&apikey=8ac30d6e')
+     .then(response => response.json())
+     .then(data => setMovies(data));
+  }, []);
+
+  console.log(movie.Title);
+
   return (
     <div className="App">
       <header className="App-header">

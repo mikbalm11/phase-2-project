143df795312208735f6b28d393d9cc14e9159e9b
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-08-21 20:51:15 -0400

    successfull fetch... again

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, {useState, useEffect} from'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { API_KEY } from './apikey';
 
-const API_URL = "https://www.omdbapi.com/?i=tt0071562&plot=full&apikey="
+const API_URL = "https://www.omdbapi.com/?&plot=full&apikey="
 
 function App() {
   const [movie, setMovie] = useState();
 
+  const example = "&i=tt0071562"
+
   useEffect(() => {
-    fetch(API_URL + API_KEY)
-     .then(response => response.json())
-     .then(data => setMovie(data))
-     .catch(error => console.error('Error:', error));
+    fetch(API_URL + API_KEY + example)
+      .then(response => response.json())
+      .then(data => setMovie(data))
+      .catch(error => console.error('Error:', error));
   }, []);
+
+  if (!movie) return <p>Movie Loading...</p>;
 
   console.log(movie.Title);
 
